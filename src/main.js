d9d154b0559cdae586aa06c5e20b1f69c9cd0483
wanghao8080 <117663444@qq.com>
2018-01-07 21:34:32 +0800

    创建 Observer 并订阅 Observable

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { Observable } from 'rxjs/Observable'
 
 const fruitsObservable = Observable.create(observer => {
   observer.next('🍎')
+  observer.next('🍊')
   observer.complete()
 })
+
+const fruitsObserver = {
+  next: data => console.log(data),
+  complete: () => console.log('done!')
+}
+
+fruitsObservable.subscribe(fruitsObserver)
