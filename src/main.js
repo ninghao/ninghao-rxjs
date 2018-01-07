841c3ce21211da53c5a081ab19e042a29acbcad6
wanghao8080 <117663444@qq.com>
2018-01-07 21:42:29 +0800

    next，error，complete

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import { Observable } from 'rxjs/Observable'
 const fruitsObservable = Observable.create(observer => {
   observer.next('🍎')
   observer.next('🍊')
+  observer.error(new Error('😭 someone took my fruit.'))
   observer.complete()
+  observer.next('🍋')
 })
 
 const fruitsObserver = {
   next: data => console.log(data),
+  error: error => console.log(error.message),
   complete: () => console.log('done!')
 }
 
