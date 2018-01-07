a085f2d3a7939c47205102cdb66dbcbd9970ff28
wanghao8080 <117663444@qq.com>
2018-01-07 21:54:04 +0800

    Operator（操作者）：from

--- src/main.js
+++ src/main.js
@@ -1,25 +1,5 @@
 import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/observable/from'
 
-const fruitsObservable = Observable.create(observer => {
-  observer.next('🍎')
-  observer.next('🍊')
-  // observer.error(new Error('😭 someone took my fruit.'))
-  setTimeout(() => {
-    observer.next('🍋')
-    observer.complete()
-  }, 2000)
-})
-
-const fruitsObserver = {
-  next: data => console.log(data),
-  error: error => console.log(error.message),
-  complete: () => console.log('done!')
-}
-
-console.log('--- before subscribe ---')
-const fruitsSubscription = fruitsObservable.subscribe(fruitsObserver)
-console.log('--- after subscribe ---')
-
-setTimeout(() => {
-  fruitsSubscription.unsubscribe()
-}, 1000)
+const observable = Observable.from(['🍎', '🍊', '🍋'])
+observable.subscribe(data => console.log(data))
