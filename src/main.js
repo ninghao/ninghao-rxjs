c1ebc5330c358fa5c80d5b8f573ab5204db71f9b
wanghao8080 <117663444@qq.com>
2018-01-07 22:02:25 +0800

    Operator（操作者）：debounceTime

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/observable/fromEvent'
 import 'rxjs/add/operator/throttleTime'
+import 'rxjs/add/operator/debounceTime'
 
 const observable = Observable.fromEvent(
   document.getElementById('search'),
   'keyup'
 )
-.throttleTime(1000)
+// .throttleTime(1000)
+.debounceTime(1000)
 
 observable.subscribe(data => console.log('searching...'))
