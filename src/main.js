c7f76a80440cee967b1130b792ac5dbba762a1b0
wanghao8080 <117663444@qq.com>
2018-01-07 21:30:05 +0800

    创建 Observable

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import { Observable } from 'rxjs/Observable'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+const fruitsObservable = Observable.create(observer => {
+  observer.next('🍎')
+  observer.complete()
 })
